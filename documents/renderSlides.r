82a54de99a3492403c13fe0324a7f78d69fea224
JMT2080AD <jason.morgan.taylor@gmail.com>
2018-04-03 20:15:17 -0700

    Init commit

---
+++ documents/renderSlides.r
@@ -0,0 +1,6 @@
+library(rmarkdown)
+decks <- list.files("./rmd", "\\.rmd$", full.names=T)
+render(decks[1], knit_root_dir="..", output_dir="./slides")
+render(decks[2], knit_root_dir="..", output_dir="./slides")
+render(decks[3], knit_root_dir="..", output_dir="./slides")
+render(decks[5], knit_root_dir="..", output_dir="./slides")
